c44fa32e29eb9e7bdafa7f8d79d9376c206c651b
SebbeCaliban <Caliban042@gmail.com>
2022-04-12 14:06:48 +0200

    Importerade saker med pybrick och skapade vissa variabler

--- project/main.py
+++ project/main.py
@@ -8,6 +8,16 @@ from pybricks.robotics import DriveBase
 import sys
 import __init__
 
+ev3 = EV3Brick()
+left_motor = Motor(Port.C)
+right_motor = Motor(Port.B)
+crane_motor = Motor(Port.A)
+robot = DriveBase(left_motor, right_motor, wheel_diameter= 56,axle_track= 118)
+Light_sensor = ColorSensor(Port.S3)
+ultrasonic  = UltrasonicSensor(Port.S4)
+touch_sensor = TouchSensor(Port.S1)
+
+
 def main():
     return 0
 
