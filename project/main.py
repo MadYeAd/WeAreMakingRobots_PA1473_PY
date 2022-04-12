6d129e4657a5ed355a7d316b98d5c07801a92aa3
SebbeCaliban <Caliban042@gmail.com>
2022-04-12 13:51:42 +0200

    main

--- project/main.py
+++ project/main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env pybricks-micropython
+from pybricks import robotics
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor, TouchSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
 import sys
 import __init__
 
