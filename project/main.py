a773f6dc0da4bf7536c4f9b07e34be7d688974ab
Hefv435 <jeff.and@icloud.com>
2022-04-12 14:27:55 +0200

    Colisonavoidanc. Whant input on 'motors_preform'.  (Jeff)

--- project/main.py
+++ project/main.py
@@ -8,6 +8,50 @@ from pybricks.robotics import DriveBase
 import sys
 import __init__
 
+motor_left = Motor(Port.C)
+motor_right = Motor(Port.B)
+
+###Jeff
+ultrasonic = UltrasonicSensor(Port.S4)
+
+
+def motors_perform(action, speed_modifier):     # Temp want input...
+    if action == "hold":
+        motor_right.hold()
+        motor_left.hold()
+    elif action == "forward":
+        motor_right.run(360 * speed_modifier)
+        motor_left.run(360 * speed_modifier)
+    elif action == "left":
+        motor_right.run(180 * speed_modifier)
+        motor_left.run(-180 * speed_modifier)
+    elif action == "right":
+        motor_right.run(-180 * speed_modifier)
+        motor_left.run(180 * speed_modifier) 
+
+#
+
+def colisionavoidenc():
+    if ultrasonic.distance() < 200 and ultrasonic.distance() > 150:
+        motors_perform("forward", 0.4)
+        print('Decreasing speed1')
+    elif ultrasonic.distance() < 150 and ultrasonic.distance() > 120:
+        motors_perform("forward", 0.3)
+        print('Decreasing speed2')
+    elif ultrasonic.distance() < 120 and ultrasonic.distance() > 100:
+        motors_perform("forward", 0.2)
+        print('Decreasing speed3')
+    elif ultrasonic.distance() < 100 and ultrasonic.distance() > 70:
+        motors_perform("forward", 0.1)
+        print('Decreasing speed4')
+    elif ultrasonic.distance() < 70:
+        motors_perform("hold", 0)
+        print('Full stop')    
+    else:
+        motors_perform("forward", 0.5)
+
+###
+
 def main():
     return 0
 
